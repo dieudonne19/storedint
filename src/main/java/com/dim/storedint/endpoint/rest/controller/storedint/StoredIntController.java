92dd77eb3d072c79cab3bd8a870350064473ca9f
dieudonne19 <hei.dieudonne.19@gmail.com>
2025-07-08 08:56:46 +0300

    fix: delete all bucket files

--- src/main/java/com/dim/storedint/endpoint/rest/controller/storedint/StoredIntController.java
+++ src/main/java/com/dim/storedint/endpoint/rest/controller/storedint/StoredIntController.java
@@ -2,19 +2,52 @@ package com.dim.storedint.endpoint.rest.controller.storedint;
 
 
 import com.dim.storedint.PojaGenerated;
-import com.dim.storedint.service.StoredIntService;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static java.io.File.createTempFile;
 
 @PojaGenerated
 @RestController
 @AllArgsConstructor
 public class StoredIntController {
-    StoredIntService storedIntService;
 
     @GetMapping("/stored-int")
     public String store_int() {
-        return storedIntService.storeInt();
+        return this.storeInt();
+    }
+
+    @SneakyThrows
+    private String storeInt() {
+        var fileSuffix = ".txt";
+        var filePrefix = "stored-int";
+        var bucketKey = filePrefix + fileSuffix;
+
+
+        var isFileExists = new File("/tmp/" + bucketKey);
+        if (Files.exists(isFileExists.toPath())) {
+            return Files.readString(isFileExists.toPath());
+        }
+
+        var fileToUpload = createTempFile(filePrefix, fileSuffix);
+
+        long randomNumber = ThreadLocalRandom.current().nextLong(10, 20);
+        writeMessageIntoFile(randomNumber, fileToUpload);
+        return Files.readString(fileToUpload.toPath());
+    }
+
+
+    private void writeMessageIntoFile(long message, File file) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write(message + "");
+        writer.close();
     }
 }
